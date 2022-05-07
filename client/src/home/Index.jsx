c1b95d01feab629e4bd89eb26807f21a4b8f50fa
Simoblaster <saturno.simone@gmail.com>
2022-05-07 12:07:36 +0200

    Update homepage

--- client/src/home/Index.jsx
+++ client/src/home/Index.jsx
@@ -8,6 +8,8 @@ import {
   faCoffee,
   faArrowLeft,
   faArrowRight,
+  faPencil,
+  faTrash
 } from "@fortawesome/free-solid-svg-icons";
 import Moment from "react-moment";
 import moment from 'moment'
@@ -17,6 +19,9 @@ function Home() {
   const [transactions, setTransactions] = useState([]);
   const [expenses, setExpenses] = useState([]);
   const [incomes, setIncomes] = useState([]);
+  const [expensesBalance, setExpensesBalance] = useState(0);
+  const [incomesBalance, setIncomesBalance] = useState(0);
+  const [monthlyBalance, setMonthlyBalance] = useState(0);
 
   const [date, setDate] = useState();
   // const [listByMonth, setListByMonth] = useState([]);
@@ -61,15 +66,19 @@ function Home() {
 
     const incomes = [];
     const expenses = [];
+    let incomesBalanceTemp = 0;
+    let expensesBalanceTemp = 0;
 
     transactions.forEach(transaction => {
       
       if (moment(transaction.date).month() === date.month() ) {
         
         if (transaction.type === "income") {
+          incomesBalanceTemp += transaction.value;
           incomes.push(transaction);
         }
         if (transaction.type === "expense") {
+          expensesBalanceTemp += transaction.value;
           expenses.push(transaction);
         }
 
@@ -77,15 +86,25 @@ function Home() {
 
     });
 
-    // if (incomes.length > 0) {
-      setIncomes(incomes);
-    // }
+    setIncomes(incomes);
+    setExpenses(expenses);
 
-    // if (expenses.length > 0) {
-      setExpenses(expenses);
-    // }
+    console.log("incomesBalanceTemp", incomesBalanceTemp); // DEBUG
+    console.log("expensesBalanceTemp", expensesBalanceTemp); // DEBUG
     
+    setIncomesBalance(incomesBalanceTemp.toFixed(2));
+    setExpensesBalance(expensesBalanceTemp.toFixed(2));
 
+    const monthlyBalance = (incomesBalanceTemp - expensesBalanceTemp);
+
+    console.log("monthlyBalance", monthlyBalance); // DEBUG
+
+
+    if (monthlyBalance !== 0) {
+      setMonthlyBalance(monthlyBalance);
+    } else {
+      setMonthlyBalance(0);
+    }
 
   }
 
@@ -153,7 +172,25 @@ function Home() {
             <div className="col-12">
               <div className="row mb-4">
                 <h2 className="col-12 col-md-6 text-center text-md-left mb-4 mb-md-0">
-                  Montly balance: $ 300
+                  Montly balance: {" "}
+                  {
+                    monthlyBalance === 0 ? (
+                      <span>
+                        ${monthlyBalance.toFixed(2)}
+                      </span>
+                    ) :
+                    monthlyBalance > 0 ? (
+                      <span className="color-green">
+                        ${monthlyBalance.toFixed(2)}
+                      </span>
+                    ) :
+                    monthlyBalance < 0 ? (
+                      <span className="color-red">
+                        ${monthlyBalance.toFixed(2)}
+                      </span>
+                    ) : 
+                    null
+                  }
                 </h2>
                 <div className="col-12 col-md-6 text-center text-md-right">
                   <button className="btn btn-danger mr-3">New expense</button>{" "}
@@ -161,64 +198,118 @@ function Home() {
                 </div>
               </div>
               <div className="row">
-                <h2 className="col-12 text-center mb-4">
-                    {/* Todo */}
-                  <FontAwesomeIcon className="cursor-pointer" onClick={() => onSwitchMonth('-')} icon={faArrowLeft} /> {date.format("MMMM yyyy")}{" "}
-                  <FontAwesomeIcon className="cursor-pointer" onClick={() => onSwitchMonth('+')} icon={faArrowRight} />
+                <h2 className="col-12 mb-4">
+                  <div className="row">
+                    <div className="col text-right m-auto">
+                      <button className="btn btn-info" onClick={() => onSwitchMonth("-")}>
+                        <FontAwesomeIcon
+                          icon={faArrowLeft}
+                        />
+                      </button>
+                    </div>
+                    <div className="col-2 text-center">
+                      {date.format("MMMM yyyy")}
+                    </div>
+                    <div className="col text-left m-auto">
+                      <button className="btn btn-info" onClick={() => onSwitchMonth("+")}>
+                        <FontAwesomeIcon
+                          icon={faArrowRight}
+                        />
+                      </button>
+                    </div>
+                  </div>
                 </h2>
                 <div className="col-12 col-md-6">
                   <div className="card bg-light mb-3">
                     <div className="card-header">
-                      <h3 className="color-red text-center">Expenses</h3>
+                      <h3 className="color-red text-center">Expenses ${expensesBalance}</h3>
                     </div>
                     <div className="card-body scrollable-card-content">
-                      {expenses.map((expense, index) => (
-                        <div key={index} className="row">
-                          <div className="col-auto m-auto">
-                            {/* test icon */}
-                            <FontAwesomeIcon icon={faCoffee} />
-                          </div>
-                          <div className="col m-auto">
-                            <div>Test Category</div>
-                            <div>
-                              <Moment format="dddd MM/DD/YYYY">
-                                {expense.updatedAt}
-                              </Moment>
+                      {expenses.length > 0 ? (
+                        expenses.map((expense, index) => (
+                          <div key={index} className="row">
+                            <div className="col-auto m-auto">
+                              {/* test icon */}
+                              <FontAwesomeIcon icon={faCoffee} />
+                            </div>
+                            <div className="col m-auto">
+                              <div>Test Category</div>
+                              <div>
+                                <Moment format="dddd MM/DD/YYYY">
+                                  {expense.updatedAt}
+                                </Moment>
+                              </div>
+                            </div>
+                            <div className="col m-auto color-red">
+                              {user.currency + expense.value}
+                            </div>
+                            <div className="col m-auto text-right">
+                              <button className="btn btn-info mr-3">
+                                <FontAwesomeIcon icon={faPencil} />
+                              </button>
+                              <button className="btn btn-danger">
+                                <FontAwesomeIcon icon={faTrash} />
+                              </button>
                             </div>
                           </div>
-                          <div className="col m-auto color-red">
-                            {user.currency + " " + expense.value}
+                        ))
+                      ) : (
+                        <div>
+                          <div>No expenses recorded for this month</div>
+                          <div>
+                            <button className="btn btn-danger mr-3">
+                              New expense
+                            </button>
                           </div>
                         </div>
-                      ))}
+                      )}
                     </div>
                   </div>
                 </div>
                 <div className="col-12 col-md-6">
                   <div className="card bg-light mb-3">
                     <div className="card-header">
-                      <h3 className="color-green text-center">Incomes</h3>
+                      <h3 className="color-green text-center">Incomes ${incomesBalance}</h3>
                     </div>
                     <div className="card-body scrollable-card-content">
-                      {incomes.map((income, index) => (
-                        <div key={index} className="row">
-                          <div className="col-auto m-auto">
-                            {/* test icon */}
-                            <FontAwesomeIcon icon={faCoffee} />
-                          </div>
-                          <div className="col m-auto">
-                            <div>Test Category</div>
-                            <div>
-                              <Moment format="dddd MM/DD/YYYY">
-                                {income.updatedAt}
-                              </Moment>
+                      {incomes.length > 0 ? (
+                        incomes.map((income, index) => (
+                          <div key={index} className="row">
+                            <div className="col-auto m-auto">
+                              {/* test icon */}
+                              <FontAwesomeIcon icon={faCoffee} />
+                            </div>
+                            <div className="col m-auto">
+                              <div>Test Category</div>
+                              <div>
+                                <Moment format="dddd MM/DD/YYYY">
+                                  {income.updatedAt}
+                                </Moment>
+                              </div>
+                            </div>
+                            <div className="col m-auto color-green">
+                              {user.currency + income.value}
+                            </div>
+                            <div className="col m-auto text-right">
+                              <button className="btn btn-info mr-3">
+                                <FontAwesomeIcon icon={faPencil} />
+                              </button>
+                              <button className="btn btn-danger">
+                                <FontAwesomeIcon icon={faTrash} />
+                              </button>
                             </div>
                           </div>
-                          <div className="col m-auto color-green">
-                            {user.currency + " " + income.value}
+                        ))
+                      ) : (
+                        <div>
+                          <div>No incomes recorded for this month</div>
+                          <div>
+                            <button className="btn btn-success ml-3">
+                              New income
+                            </button>
                           </div>
                         </div>
-                      ))}
+                      )}
                     </div>
                   </div>
                 </div>
